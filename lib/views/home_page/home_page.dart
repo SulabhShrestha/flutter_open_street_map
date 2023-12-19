7416f2f830407ae270af6ed7aaea4172747ac043
Sulabh Shrestha <shresthasulabh777@gmail.com>
2023-12-19 15:14:07 +0545

    user can now select locations

--- lib/views/home_page/home_page.dart
+++ lib/views/home_page/home_page.dart
@@ -1,9 +1,20 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  // location that the user has selected
+  List<LatLng> selectedLocations = [];
 
   @override
   Widget build(BuildContext context) {
@@ -13,16 +24,34 @@ class HomePage extends StatelessWidget {
       ),
 
       body: FlutterMap(
-        options: const MapOptions(
-          initialCenter: LatLng(26.5, 88.07),
+        options:  MapOptions(
+          onLongPress: (position, latlng){
+            selectedLocations.add(latlng);
+            setState(() {});
+          },
+          initialCenter: LatLng(26.5, 88.0),
           initialZoom: 12.0,
           maxZoom: 20.0,
+
         ),
         children: [
+
           TileLayer(
             urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-            subdomains: const ['a', 'b', 'c'],
           ),
+
+          for(var selectedLocation in selectedLocations)
+          MarkerLayer(
+            markers: [
+              Marker(
+                point: selectedLocation,
+                width: 80,
+                height: 80,
+                child: Icon(Icons.location_on_outlined, color: Colors.red),
+              ),
+            ],
+          ),
+
         ],
       ),
     );
